f28e58f6cac24ca85dfdd208edb04ca9872f9fb0
Darryl Mendonez <dmendonez@gmail.com>
2016-07-01 18:52:55 -0400

    finished tutorial 13 on ng-click and got buttons to delete when clicking on x button

--- public/app/app.js
+++ public/app/app.js
@@ -2,7 +2,10 @@ var myNinjaApp = angular.module('myNinjaApp', []);
 
 myNinjaApp.controller('NinjaController', ['$scope', function($scope) {
 
-  $scope.message = "hey y'all";
+  $scope.removeNinja = function(ninja) {
+    var removedNinja = $scope.ninjas.indexOf(ninja);
+    $scope.ninjas.splice(removedNinja, 1);
+  };
 
   $scope.ninjas = [
     {
