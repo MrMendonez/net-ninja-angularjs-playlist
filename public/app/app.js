7060d51882f850641692f9e76ec62a285fbb444f
Darryl Mendonez <dmendonez@gmail.com>
2016-07-01 17:04:41 -0400

    finished tutorial 10. added filters including orderBy, search, and currency

--- public/app/app.js
+++ public/app/app.js
@@ -4,6 +4,27 @@ myNinjaApp.controller('NinjaController', ['$scope', function($scope) {
 
   $scope.message = "hey y'all";
 
-  $scope.ninjas = ['yoshi', 'crystal', 'ryu', 'shaun'];
+  $scope.ninjas = [
+    {
+      name: "Yoshi",
+      belt: "green",
+      rate: 50
+    },
+    {
+      name: "Crystal",
+      belt: "yellow",
+      rate: 30
+    },
+    {
+      name: "Ryu",
+      belt: "orange",
+      rate: 10
+    },
+    {
+      name: "Shaun",
+      belt: "black",
+      rate: 1000
+    }
+  ];
 
 }]);
